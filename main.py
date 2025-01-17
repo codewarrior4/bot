eb402e0eae3c8de6d13a65c3618a54d01ae50153
codewarrior4 <mayowatope119@gmail.com>
2025-01-17 03:44:37 -0800

    remved burna

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from file1 import setup_application as setup_application_file1
 from file2 import setup_application as setup_application_file2
-from file3 import setup_application as setup_application_file3
+# from file3 import setup_application as setup_application_file3
 from file4 import setup_application as setup_application_file4
 
 async def start_bot(app):
@@ -12,11 +12,12 @@ async def start_bot(app):
 async def main():
     app1 = setup_application_file1()
     app2 = setup_application_file2()
-    app3 = setup_application_file3()
+    # app3 = setup_application_file3()
     app4 = setup_application_file4()
     # Run both bots concurrently
     # await asyncio.gather(start_bot(app2))
-    await asyncio.gather(start_bot(app1), start_bot(app2), start_bot(app3), start_bot(app4))
+    # await asyncio.gather(start_bot(app1), start_bot(app2), start_bot(app3), start_bot(app4))
+    await asyncio.gather(start_bot(app1), start_bot(app2), start_bot(app4))
     # Keep the program running
     await asyncio.Event().wait() 
 
